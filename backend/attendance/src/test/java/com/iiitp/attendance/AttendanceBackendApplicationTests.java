5ed76bb6e9f6fc5b5aad54c32b40bebc06a590e8
Archit-P-Singh <architps456@gmail.com>
2025-09-26 01:58:52 +0530

    Created and setup frontend and backend directories

---
+++ backend/attendance/src/test/java/com/iiitp/attendance/AttendanceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iiitp.attendance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
